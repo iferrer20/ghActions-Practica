4b2817f9785f20c34add55b3b13e8ca3640d09b6
iferrer20 <iferreriestacio@gmail.com>
2021-12-19 18:59:10 +0100

    testing actions

---
+++ .github/actions/badges/badges.py
@@ -0,0 +1,9 @@
+import sys
+import re
+
+content = open("README.md", "r").read()
+f = open("README.md", "w")
+f.write(
+    re.sub('tested%20with-Cypress-04C38E|test-failure-red', 'tested%20with-Cypress-04C38E' if sys.argv[1] == "success" else 'test-failure-red', content)
+)
+f.close()
