4dea72ff37bab9d3932a80208a1698047d5a890c
iferrer20 <iferreriestacio@gmail.com>
2021-12-19 19:27:38 +0100

    trying to fix badge action

--- .github/actions/badges/badges.py
+++ .github/actions/badges/badges.py
@@ -1,7 +1,7 @@
 import sys
 import re
 
-content = open("README.md", "r").read()
+content = open("README.md").read()
 f = open("README.md", "w")
 f.write(
     re.sub('tested%20with-Cypress-04C38E|test-failure-red', 'tested%20with-Cypress-04C38E' if sys.argv[1] == "success" else 'test-failure-red', content)
