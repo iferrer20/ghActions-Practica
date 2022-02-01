412b4ccf71868d2cecf3af4714f04aac4a687c7d
iferrer20 <iferreriestacio@gmail.com>
2022-02-01 16:02:17 +0100

    Nogithub

--- .github/actions/badges/badges.py
+++
@@ -1,9 +0,0 @@
-import sys
-import re
-
-content = open("README.md").read()
-f = open("README.md", "w")
-f.write(
-    re.sub('tested%20with-Cypress-04C38E|test-failure-red', 'tested%20with-Cypress-04C38E' if sys.argv[1] == "success" else 'test-failure-red', content)
-)
-f.close()
