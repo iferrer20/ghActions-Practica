ac3c7fe6a3a61c7b5f693f52028cb9c317605f9b
iferrer20 <iferreriestacio@gmail.com>
2022-02-01 16:52:31 +0100

    jenkins

--- jenkinsScripts/push.sh
+++ jenkinsScripts/push.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo AAAAAAAAAAA${3}
-git remote set-url origin https://${1}:${3}@github.com/iferrer20/ghActions-Practica
+echo AAAAAAAAAAA${TOKEN}
+git remote set-url origin https://iferrer20:${TOKEN}@github.com/iferrer20/ghActions-Practica
 git add .
-git config --global user.name "${1}"
-git config --global user.email "${1}@users.noreply.github.com"
-git commit -m "${2}" --allow-empty
+git config --global user.name "iferrer20"
+git config --global user.email "iferrer20@users.noreply.github.com"
+git commit -m "jenkins_autocomit" --allow-empty
 git push origin HEAD:jenkins
